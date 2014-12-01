9a7a419f1e0ad23920d403ba29f0e47a02dd61a6
Najderas <spam.gienek@gmail.com>
2014-12-01 13:10:55 +0100

    initial class Timer

---
+++ src/to2/dice/controllers/poker/Timer.java
@@ -0,0 +1,19 @@
+package to2.dice.controllers.poker;
+
+import to2.dice.game.Player;
+
+/**
+ * Created by Grzegorz on 2014-12-01.
+ */
+public class Timer implements Runnable {
+
+    public Timer(Player player, int timeForMove,  ) {
+
+    }
+
+
+    @Override
+    public void run() {
+
+    }
+}
