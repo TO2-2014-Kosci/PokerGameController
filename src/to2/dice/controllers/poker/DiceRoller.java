33856498209876b6299f53394ad3dce78b624ccf
jlabuz <vaduz16@gmail.com>
2014-12-08 21:20:25 +0100

    reimplemented PokerGameController; GameThread and RoomController interfaces

--- src/to2/dice/controllers/poker/DiceRoller.java
+++ src/to2/dice/controllers/poker/DiceRoller.java
@@ -1,19 +1,21 @@
 package to2.dice.controllers.poker;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import to2.dice.game.Dice;
 import java.util.Random;
 
 public class DiceRoller {
 
-    private Dice getRandomDice() {
-        Dice dice = new Dice(settings.getDiceNumber());
-        int[] dices = new int[settings.getDiceNumber()];
-        Random generator = new Random();
-        for(int i = 0; i < settings.getDiceNumber(); i++) {
-            dices[i] = generator.nextInt(6)+1;
-        }
-        dice.setDice(dices);
-        return dice;
+    public DiceRoller(int diceNumber) {
+
+    }
+
+    public int rollSingleDice() {
+        throw new NotImplementedException();
+    }
+
+    public Dice rollDice() {
+        throw new NotImplementedException();
     }
 
 }
